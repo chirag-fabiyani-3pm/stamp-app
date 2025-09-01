8d4eed954d753cc3829ac50c00e5c7161dd103aa
Chirag Fabiyani <chirag.fabiyani@decoded.digital>
2025-09-02 03:08:05 +0530

    Updated

--- lib/api/stamp-master-catalog.ts
+++ lib/api/stamp-master-catalog.ts
@@ -230,7 +230,7 @@ type StampMasterCatalogResponse = {
   hasNextPage: boolean
 }
 
-const BASE_URL = 'https://decoded-app-stamp-api-prod-01.azurewebsites.net/api/v1/StampMasterCatalog'
+const BASE_URL = 'https://decoded-app-stamp-api-prod-01.azurewebsites.net/api/v1/StampMasterCatalog/Published'
 const JWT_COOKIE_NAME = 'jwt'
 
 export async function fetchStampMasterCatalogPage(params: { pageNumber: number, pageSize: number, catalogExtractionProcessId: string, jwt?: string }): Promise<StampMasterCatalogResponse> {
